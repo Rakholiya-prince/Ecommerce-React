4f97e2cfb7d80d220eb8373200c1220b83139fb6
VirAt-k0hLi-18 <rakholiyaprince13@gmail.com>
2025-02-02 17:34:38 +0530

    e-site-1

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+function DescriptionBox() {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          minus distinctio qui earum eum ratione illo dolorum ex ipsa maiores
+          dolorem vel unde repellat sunt, eius optio, eaque eligendi similique
+          quo repellendus, libero mollitia. Qui voluptates maxime reprehenderit
+          nihil consectetur illo sit sed, possimus incidunt asperiores nam
+          atque! Delectus fugiat alias, officiis nesciunt eaque ab, veniam
+          deleniti incidunt error sint distinctio quam fuga inventore pariatur
+          ipsam nam rem quis excepturi odio fugit accusantium adipisci ullam
+          possimus qui. Consectetur beatae fugiat facilis quo! Vitae pariatur
+          sit beatae recusandae necessitatibus in assumenda dolor, nesciunt ex!
+          Beatae cumque sequi facilis, veritatis distinctio iste.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
+          mollitia incidunt necessitatibus, omnis earum dolorem at reprehenderit
+          eos doloremque nisi tempore ratione eligendi repudiandae reiciendis?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default DescriptionBox;
